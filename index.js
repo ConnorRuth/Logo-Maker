c343ebd99f5aa77335cb200ed1efd45a051e43bf
ConnorRuth <152222829+ConnorRuth@users.noreply.github.com>
2024-02-29 10:59:38 -0800

    added base code

---
+++ index.js
@@ -0,0 +1,47 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const logoGen = require("./lib/logoGen");
+
+const questions = [
+    {
+        type: "input",
+        message: "Please type up to three letters you would like to include.",
+        name: "text"
+    },
+    {
+        type: "input",
+        message: "What color would you like the text to be?",
+        name: "textColor"
+    },
+    {
+        type: "list",
+        message: "What shape would you like to include?",
+        name: "shape",
+        choices: ["Circle", "Triangle", "Square"]
+    },
+    {
+        type: "input",
+        message: "What color would you like the shape to be?",
+        name: "shapeColor"
+    },
+    {
+        type: "input",
+        message: "what name woud you like to have?(it will be the svg file type)",
+        name: "fileName"
+    }
+]
+
+function writeToFile(data){
+    let logo = logoGen(data);
+    fs.writeFile(`./examples/${data.fileName}.svg`, logo, (err) =>
+    err ? console.log(err) : console.log('Successfully created logo check examples for your logo!')
+    );
+}
+function init() {
+    inquirer.prompt(questions)
+    .then((data) => {
+        writeToFile(data);
+    })
+    
+}
+init();
